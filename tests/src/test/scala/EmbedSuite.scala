2ef25c5bbb21ff6b98eba174484e0627b5275e66
Denys Shabalin <denys.shabalin@epfl.ch>
2015-05-13 12:19:59 +0200

    More tests for @embed

--- tests/src/test/scala/EmbedSuite.scala
+++ tests/src/test/scala/EmbedSuite.scala
@@ -3,34 +3,37 @@ package test
 import org.scalatest.FunSuite
 import offheap._
 
-@data class Emb1(x: Int, y: Long)
-@data class Emb2(x: Byte, @embed emb1: Emb1)
-
-@data class Emb3(x: Int, y: Short)
-@data class Emb4(x: Byte, @embed emb3: Emb3, y: Long)
+@data class Inner(var v: Int)
+@data class Outer(@embed var inner: Inner)
 
 class EmbedSuite extends FunSuite {
+  implicit val alloc = Allocator()
 
-  test("Emb1.x    offset") { assert(offsetOf[Emb1]("x") == 0) }
-  test("Emb1.y    offset") { assert(offsetOf[Emb1]("y") == 8) }
+  test("inner pointer") {
+    val inner = Inner(42)
+    val outer = Outer(inner)
+    assert(Outer.toAddr(outer) == Inner.toAddr(outer.inner))
+  }
 
-  test("Emb2.x    offset") { assert(offsetOf[Emb2]("x") == 0) }
-  test("Emb2.emb1 offset") { assert(offsetOf[Emb2]("emb1") == 8) }
+  test("modify after copy") {
+    val outer = Outer(Inner(42))
+    assert(outer.inner.v == 42)
+    val outer2 = outer.copy()
+    assert(outer2.inner.v == 42)
+    outer2.inner.v = 43
+    assert(outer.inner.v == 42)
+    assert(outer2.inner.v == 43)
+  }
 
-  test("Emb3.x    offset") { assert(offsetOf[Emb3]("x") == 0) }
-  test("Emb3.y    offset") { assert(offsetOf[Emb3]("y") == 4) }
-
-  test("Emb4.x    offset") { assert(offsetOf[Emb4]("x") == 0) }
-  test("Emb4.emb3 offset") { assert(offsetOf[Emb4]("emb3") == 4) }
-  test("Emb4.y    offset") { assert(offsetOf[Emb4]("y") == 16) }
-
-  test("Emb1 data alignment") { assert(alignmentOfData[Emb1] == 8) }
-  test("Emb2 data alignment") { assert(alignmentOfData[Emb2] == 8) }
-  test("Emb3 data alignment") { assert(alignmentOfData[Emb3] == 4) }
-  test("Emb4 data alignment") { assert(alignmentOfData[Emb4] == 8) }
-
-  test("sizeOfData[Emb1]") { assert(sizeOfData[Emb1] == 16) }
-  test("sizeOfData[Emb2]") { assert(sizeOfData[Emb2] == 24) }
-  test("sizeOfData[Emb3]") { assert(sizeOfData[Emb3] == 6) }
-  test("sizeOfData[Emb4]") { assert(sizeOfData[Emb4] == 24) }
+  test("assign embedded") {
+    val outer = Outer(Inner(42))
+    assert(outer.inner.v == 42)
+    val inner = Inner(43)
+    outer.inner = inner
+    assert(inner.v == 43)
+    assert(outer.inner.v == 43)
+    inner.v = 44
+    assert(inner.v == 44)
+    assert(outer.inner.v == 43)
+  }
 }
