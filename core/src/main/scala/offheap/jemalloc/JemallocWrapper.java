45eda4174d395363180a740a8a75186af4447ac1
Adam Rosenberger <adam.rosenberger1@gmail.com>
2015-06-05 10:54:17 -0500

    Checkpoint commit for linux build - primary machine just broke and I need to reimage

---
+++ core/src/main/scala/offheap/jemalloc/JemallocWrapper.java
@@ -0,0 +1,24 @@
+package offheap.jemalloc;
+
+public final class JemallocWrapper {
+    static {
+        System.loadLibrary("jemallocWrapper");
+    }
+
+    public static long malloc(long size) {
+        return malloc_0(size);
+    }
+
+    public static long realloc(long address, long newSize) {
+        return realloc_0(address, newSize);
+    }
+
+    public static void free(long address) {
+        free_0(address);
+    }
+
+    private static native long malloc_0(long size);
+    private static native long realloc_0(long address, long newSize);
+    private static native void free_0(long address);
+}
+
