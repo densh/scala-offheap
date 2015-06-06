8afe7c18eafa8f16d1d2c3972b2d3542854c96fc
Adam Rosenberger <adam.rosenberger1@gmail.com>
2015-06-06 18:09:51 -0500

    Fix mutable test failure from having multiple versions of the suite running

--- tests/src/test/scala/MutableSuite.scala
+++ tests/src/test/scala/MutableSuite.scala
@@ -1,30 +1,45 @@
 package test
 
-import org.scalatest.FunSuite
 import offheap._
+import org.scalatest.{BeforeAndAfterEach, FunSuite}
 
 @data class C1(var x: Int)
 
 @data class C2 { var x: Int = 2 }
 
-@data class C3 { C3.x = 10 }
-object C3 { var x = 0 }
+@data class C3 {
+  C3.x = 10
+}
+
+object C3 {
+  var x = 0
+
+  def reset(): Unit = {
+    x = 0
+  }
+}
 
 @data class C4 {
   C4.before = x
   val x: Int = 42
   C4.after = x
 }
+
 object C4 {
   var before = 0
   var after = 0
+
+  def reset(): Unit = {
+    before = 0
+    after = 0
+  }
 }
 
 @data class C5 {
   var x: Long = _
 }
 
-abstract class MutableSuite extends FunSuite { provider: HasAllocator =>
+abstract class MutableSuite extends FunSuite with BeforeAndAfterEach { provider: HasAllocator =>
   implicit val alloc = provider.allocator()
 
   test("mutable constructor argument") {
@@ -55,6 +70,11 @@ abstract class MutableSuite extends FunSuite { provider: HasAllocator =>
 
   test("default init var") {
     assert(C5().x == 0L)
+  }
+
+  override protected def beforeEach(): Unit = {
+    C3.reset()
+    C4.reset()
   }
 }
 
