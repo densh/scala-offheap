dd75776231719b30693b145c3712a14066afe587
Denys Shabalin <denys.shabalin@epfl.ch>
2015-06-08 19:07:07 +0200

    Much more comprehensive allocation benchmark
           (impl)   (n)         Score        Error  Units
      pool-region    16  28223792.735 ± 121939.641  ops/s
      pool-region    64  26578928.215 ±  27858.649  ops/s
      pool-region   256  26323559.824 ±  82098.688  ops/s
      pool-region  1024  15211234.752 ± 637427.334  ops/s
      pool-region  4096   2798810.489 ±  91252.868  ops/s
    direct-region    16   2596446.722 ±  23753.075  ops/s
    direct-region    64   2471177.225 ±  24528.221  ops/s
    direct-region   256   2920437.227 ± 247373.672  ops/s
    direct-region  1024   1969807.860 ±  94447.404  ops/s
    direct-region  4096    721953.571 ±   6090.575  ops/s
           malloc    16   9965850.160 ±  20762.977  ops/s
           malloc    64   9937962.136 ±  20320.780  ops/s
           malloc   256   8198536.187 ± 248588.526  ops/s
           malloc  1024   9968047.981 ±   5173.931  ops/s
           malloc  4096   9868782.648 ±  13128.168  ops/s
               gc    16  71141846.089 ±  50143.346  ops/s
               gc    64  69899831.422 ±  40409.449  ops/s
               gc   256  31961056.816 ±  33130.217  ops/s
               gc  1024   7596544.396 ±  12725.289  ops/s
               gc  4096   3236928.365 ±   4169.415  ops/s

--- jmh/src/main/scala/Allocation.scala
+++ jmh/src/main/scala/Allocation.scala
@@ -4,29 +4,61 @@ import org.openjdk.jmh.annotations._
 import java.util.concurrent.TimeUnit
 import offheap._
 
-class Point(val x: Int, val y: Int)
-
-@data class OffheapPoint(val x: Int, val y: Int)
+class JAlloc8(val a: Long)
+class JAlloc16(val a: Long, val b: Long)
+class JAlloc32(val a: Long, val b: Long, val c: Long, val d: Long)
+class JAlloc64(val a: Long, val b: Long, val c: Long, val d: Long,
+               val e: Long, val f: Long, val g: Long, val h: Long)
 
 @State(Scope.Thread)
 class OffheapAllocation {
-  implicit val props = Region.Props()
-  var r: Region = _
+  val pool = Pool(malloc)
+  @Param(scala.Array("malloc"))
+  var impl: String = _
+  @Param(scala.Array("16", "64", "256", "1024", "4096"))
+  var n: Int = _
+
+  var alloc: Allocator = _
+  var addr: Addr = _
+  var disposeAlloc: () => Unit = _
+  var disposeAddr: Addr => Unit = _
 
   @Setup(Level.Iteration)
-  def setup(): Unit = r = Region.open
+  def setup(): Unit = {
+    val (a, d1, d2) = impl match {
+      case "malloc"        =>
+        (malloc, () => (), malloc.free(_))
+      case "pool-region"   =>
+        val r = PoolRegion.open(pool)
+        (r, () => r.close, (addr: Addr) => ())
+      case "direct-region" =>
+        val r = DirectRegion.open(malloc)
+        (r, () => r.close, (addr: Addr) => ())
+    }
+    alloc = a
+    disposeAlloc = d1
+    disposeAddr = d2
+  }
 
   @TearDown(Level.Iteration)
-  def tearDown(): Unit =
-    r.close()
+  def tearDownIteration(): Unit = disposeAlloc()
+
+  @TearDown(Level.Invocation)
+  def tearDownInvocation(): Unit = disposeAddr(addr)
 
   @Benchmark
-  def offheapPoint() =
-    OffheapPoint(10, 20)(r)
+  def allocate =
+    // compensate for 8 byte header to get n-byte allocation
+    addr = Array.uninit[Byte](n - 8)(alloc).addr
 }
 
+@State(Scope.Thread)
 class GCAllocation {
+  @Param(scala.Array("16", "64", "256", "1024", "4096"))
+  var n: Int = _
+
   @Benchmark
-  def point() =
-    new Point(10, 20)
+  def allocate =
+    // compensate for 12 byte header to get n-byte allocation
+    new scala.Array[Byte](n - 12)
 }
