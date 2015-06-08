112f9389052384a3860106b01edcb1583aba8d80
Denys Shabalin <denys.shabalin@epfl.ch>
2015-06-08 14:07:19 +0200

    Array index should have it's own type alias

--- core/src/main/scala/offheap/Array.scala
+++ core/src/main/scala/offheap/Array.scala
@@ -16,8 +16,8 @@ final class Array[A] private (val addr: Addr) extends AnyVal {
   def nonEmpty: Boolean                                  = macro macros.ArrayApi.nonEmpty
   def size: Array.Size                                   = macro macros.ArrayApi.size
   def length: Array.Size                                 = macro macros.ArrayApi.size
-  def apply(index: Addr): A                              = macro macros.ArrayApi.apply
-  def update(index: Addr, value: A): Unit                = macro macros.ArrayApi.update
+  def apply(index: Array.Index): A                       = macro macros.ArrayApi.apply
+  def update(index: Array.Index, value: A): Unit         = macro macros.ArrayApi.update
   def foreach(f: A => Unit): Unit                        = macro macros.ArrayApi.foreach
   def map[B](f: A => B)(implicit a: Allocator): Array[B] = macro macros.ArrayApi.map[B]
   def toArray: scala.Array[A]                            = macro macros.ArrayApi.toArray
@@ -28,16 +28,20 @@ final class Array[A] private (val addr: Addr) extends AnyVal {
     else super.toString
 }
 object Array {
+  type Index = Int
   type Size = Int
-  def uninit[T](n: Size)(implicit a: Allocator): Array[T]    = macro macros.ArrayModule.uninit[T]
-  def apply[T](values: T*)(implicit a: Allocator): Array[T]  = macro macros.ArrayModule.vararg[T]
-  def fill[T](n: Size)(elem: => T)
-             (implicit a: Allocator): Array[T]               = macro macros.ArrayModule.fill[T]
-  def copy[T](from: Array[T], fromIndex: Addr,
-              to: Array[T], toIndex: Addr, size: Size): Unit = macro macros.ArrayModule.copy[T]
+  def uninit[T](n: Array.Size)
+               (implicit a: Allocator): Array[T]      = macro macros.ArrayModule.uninit[T]
+  def apply[T](values: T*)
+              (implicit a: Allocator): Array[T]       = macro macros.ArrayModule.vararg[T]
+  def fill[T](n: Array.Size)(elem: => T)
+             (implicit a: Allocator): Array[T]        = macro macros.ArrayModule.fill[T]
+  def copy[T](from: Array[T], fromIndex: Array.Index,
+              to: Array[T], toIndex: Array.Index,
+              size: Array.Size): Unit                 = macro macros.ArrayModule.copy[T]
   def fromArray[T](arr: scala.Array[T])
-                  (implicit a: Allocator): Array[T]          = macro macros.ArrayModule.fromArray[T]
+                  (implicit a: Allocator): Array[T]   = macro macros.ArrayModule.fromArray[T]
 
-  def empty[T]: Array[T]                                     = new Array[T](0L)
-  def fromAddr[T](addr: Addr): Array[T]                      = new Array[T](addr)
+  def empty[T]: Array[T]                              = new Array[T](0L)
+  def fromAddr[T](addr: Addr): Array[T]               = new Array[T](addr)
 }
