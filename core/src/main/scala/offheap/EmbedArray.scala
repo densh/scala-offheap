dfce5c90b0cd114e0e1aff3275ad2187961da300
Florian Chlan <florian@chlan.net>
2015-06-10 12:43:50 +0200

    EmbedArray index should have it's own type alias

--- core/src/main/scala/offheap/EmbedArray.scala
+++ core/src/main/scala/offheap/EmbedArray.scala
@@ -12,8 +12,8 @@ final class EmbedArray[A] private (val addr: Addr) extends AnyVal {
   def nonEmpty: Boolean                                       = macro macros.EmbedArrayApi.nonEmpty
   def size: EmbedArray.Size                                   = macro macros.EmbedArrayApi.size
   def length: EmbedArray.Size                                 = macro macros.EmbedArrayApi.size
-  def apply(index: Addr): A                                   = macro macros.EmbedArrayApi.apply
-  def update(index: Addr, value: A): Unit                     = macro macros.EmbedArrayApi.update
+  def apply(index: EmbedArray.Index): A                                   = macro macros.EmbedArrayApi.apply
+  def update(index: EmbedArray.Index, value: A): Unit                     = macro macros.EmbedArrayApi.update
   def foreach(f: A => Unit): Unit                             = macro macros.EmbedArrayApi.foreach
   def map[B](f: A => B)(implicit a: Allocator): EmbedArray[B] = macro macros.EmbedArrayApi.map[B]
   def toArray: scala.Array[A]                                 = macro macros.EmbedArrayApi.toArray
@@ -24,16 +24,20 @@ final class EmbedArray[A] private (val addr: Addr) extends AnyVal {
     else super.toString
 }
 object EmbedArray {
+  type Index = Int
   type Size = Int
-  def uninit[T](n: Size)(implicit a: Allocator): EmbedArray[T]    = macro macros.EmbedArrayModule.uninit[T]
-  def apply[T](values: T*)(implicit a: Allocator): EmbedArray[T]  = macro macros.EmbedArrayModule.vararg[T]
-  def fill[T](n: Size)(elem: => T)
-             (implicit a: Allocator): EmbedArray[T]               = macro macros.EmbedArrayModule.fill[T]
-  def copy[T](from: EmbedArray[T], fromIndex: Addr,
-              to: EmbedArray[T], toIndex: Addr, size: Size): Unit = macro macros.EmbedArrayModule.copy[T]
+  def uninit[T](n: EmbedArray.Size)
+               (implicit a: Allocator): EmbedArray[T]         = macro macros.EmbedArrayModule.uninit[T]
+  def apply[T](values: T*)
+              (implicit a: Allocator): EmbedArray[T]          = macro macros.EmbedArrayModule.vararg[T]
+  def fill[T](n: EmbedArray.Size)(elem: => T)
+             (implicit a: Allocator): EmbedArray[T]           = macro macros.EmbedArrayModule.fill[T]
+  def copy[T](from: EmbedArray[T], fromIndex: EmbedArray.Index,
+              to: EmbedArray[T], toIndex: EmbedArray.Index,
+              size: EmbedArray.Size): Unit                    = macro macros.EmbedArrayModule.copy[T]
   def fromArray[T](arr: scala.Array[T])
-                  (implicit a: Allocator): EmbedArray[T]          = macro macros.EmbedArrayModule.fromArray[T]
+                  (implicit a: Allocator): EmbedArray[T]      = macro macros.EmbedArrayModule.fromArray[T]
 
-  def empty[T]: EmbedArray[T]                                     = new EmbedArray[T](0L)
-  def fromAddr[T](addr: Addr): EmbedArray[T]                      = new EmbedArray[T](addr)
+  def empty[T]: EmbedArray[T]                                 = new EmbedArray[T](0L)
+  def fromAddr[T](addr: Addr): EmbedArray[T]                  = new EmbedArray[T](addr)
 }
