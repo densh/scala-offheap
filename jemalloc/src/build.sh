0870271b44965b5284ecfe5ed4cd41a7e7a61592
Adam Rosenberger <arosenbe@kcg.com>
2015-06-19 15:40:20 -0500

    Change jemalloc working directory from tmp to target in local repo

--- jemalloc/src/build.sh
+++ jemalloc/src/build.sh
@@ -2,12 +2,13 @@
 
 set -ex
 
-CWD=$PWD
+ROOT=$PWD
 
-cd /tmp
+cd ../target
+WORK=$PWD
 
-rm -rf /tmp/usr
-mkdir -p /tmp/usr
+rm -rf $WORK/usr
+mkdir -p $WORK/usr
 
 rm -rf jemalloc_latest
 mkdir -p jemalloc_latest
@@ -18,13 +19,13 @@ git clone https://github.com/jemalloc/jemalloc.git
 cd jemalloc
 
 autoconf
-./configure --prefix=/tmp/usr --with-jemalloc-prefix=je
+./configure --prefix=$WORK/usr --with-jemalloc-prefix=je
 sed -i 's/^CFLAGS := \(.*\)/CFLAGS := -fPIC \1/g' Makefile
 
 make -j
 make install_bin install_include install_lib
 
-cd $CWD
+cd $ROOT
 cd c/
 make clean
 make
