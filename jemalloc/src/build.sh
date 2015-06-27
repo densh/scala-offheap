b1ca7e3ae9c292792017f789161477d918d37f22
Adam Rosenberger <arosenbe@kcg.com>
2015-06-26 19:12:22 -0500

    Checkpoint commit so I can work from home - Change jemalloc to dynamic instead of statically linked library and include it in src/main/resources - Unpack both jemalloc and the jni bindings and set their permissions - Some debugging / printing code for this is fails still in Travis - Change travis script to hardcoded ld_library_path for now to see if that gets us up and running, can clean up location of unpacked libraries later

--- jemalloc/src/build.sh
+++ jemalloc/src/build.sh
@@ -12,12 +12,15 @@ WORK=$PWD
 rm -rf $WORK/usr
 mkdir -p $WORK/usr
 
-rm -rf jemalloc_latest
-mkdir -p jemalloc_latest
+if [ -d "jemalloc_latest" ]; then
+    cd jemalloc_latest
+    git clean -xfd
+    git pull
+else
+    mkdir -p jemalloc_latest
+    git clone https://github.com/jemalloc/jemalloc.git
+fi
 
-cd jemalloc_latest
-
-git clone https://github.com/jemalloc/jemalloc.git
 cd jemalloc
 
 autoconf
@@ -28,7 +31,14 @@ make -j
 make install_bin install_include install_lib
 
 cd $ROOT
+
+rm -rf main/resources/*
+cp ../target/usr/lib/libjemalloc.so.2 main/resources/
+
 cd c/
 make clean
 make
 mv *.so ../main/resources
+
+cd $ROOT
+rm -rf ../target/usr
